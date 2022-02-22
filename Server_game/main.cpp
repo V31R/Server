4c755064a797ce2edef1e57abe0ee1441de30686
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2022-02-22 18:31:46 +0300

    Init commit

---
+++ Server_game/main.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Server!");
+    sf::CircleShape shape(100.f, 6);
+    shape.setFillColor(sf::Color::Yellow);
+
+    sf::UdpSocket socket;
+
+    if (socket.bind(54000) != sf::Socket::Done)
+    {
+        // error...
+        shape.setFillColor(sf::Color::Red);
+    }
+    window.clear();
+    window.draw(shape);
+    window.display();
+
+    char data[100];
+    std::size_t received;
+    sf::IpAddress sender;
+    unsigned short port;
+    if (socket.receive(data, 100, received, sender, port) != sf::Socket::Done)
+    {
+        // error...
+        shape.setFillColor(sf::Color::Red);
+    }
+    else {
+
+        shape.setFillColor(sf::Color::Green);
+
+    }
+    std::cout << "Received " << received << " bytes from " << sender << " on port " << port << std::endl;
+    printf("%s", data);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
